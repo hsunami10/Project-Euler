b52a654872ff71f75b62ab045947aee7b44b92f1
Michael Hsu <michael100.hsu@gmail.com>
2017-01-15 15:20:36 -0600

    finished problem 9, stuck on problem 8

---
+++ Problem 8/problem8.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+// Problem 8 (Project Euler)
+
+using namespace std;
+
+// Can't store 1000 digit number
+int main() {
+  
+  int array[76][13];
+  long oldProduct = 0;
+  long newProduct = 0;
+
+ string stringNum = to_string(num);
+
+ cout << stringNum;
+  for(int i = 0; i < 987; i++) {
+    
+  }
+}
