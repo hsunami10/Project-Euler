746e714ee81a36484032f066e35a01f01d8c84eb
Michael Hsu <michael100.hsu@gmail.com>
2017-01-15 00:05:30 -0600

    revised problem 3

--- Problem 3/problem3.cpp
+++ Problem 3/problem3.cpp
@@ -12,26 +12,25 @@ int main() {
   long factor = 0;
   long factorPrime = 0;
   long number = 600851475143;
-  int end = 0;
 
   for(long i = 2; i <= number; i++) {
 
     // Check if factor
     if(number % i == 0) {
       factor = i;
-      
-      // Loop through 2-factor
-      for(int j = 2; j < factor; j++) {
+    }      
+     // Loop through 2-factor
+     for(int j = 2; j < factor; j++) {
 	
-	// If not a prime
-	if(factor % j == 0) {
-	  break;
-	}
-	else {
-	  factorPrime = factor;
-	}
-      }
-    }
-  }
+       // If not a prime
+       if(factor % j == 0) {
+	 break;
+       }
+       else {
+         factorPrime = factor;
+         cout << "prime " << factorPrime << '\n';
+       }
+     }
+   }
   cout << factorPrime << '\n';
 }
